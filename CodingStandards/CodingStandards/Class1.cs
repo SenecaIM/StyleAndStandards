2d162f1aff144ec70ab7b2eb0b081c11c2e58af1
Kacper <kacper.lagowski@senecaim.com>
2016-06-08 14:19:37 +0100

    Pushing the right files in.

--- CodingStandards/CodingStandards/Class1.cs
+++ CodingStandards/CodingStandards/Class1.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Coding_Standard
+namespace CodingStandards
 {
-    class Iterations_and_Loops
+    public class Class1
     {
     }
 }
