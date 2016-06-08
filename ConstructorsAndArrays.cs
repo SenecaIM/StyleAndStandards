368034e51b1bc3ae8cc70bddce2800fa35c34c79
Kacper <kacper.lagowski@senecaim.com>
2016-06-08 14:34:45 +0100

    Name Changing

---
+++ ConstructorsAndArrays.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Standard
+{
+    class Constructors_and_arrays
+    {
+        //Constructor example
+        
+        public Employee(int weeklySalary, int numberOfWeeks)
+        {
+            salary = weeklySalary * numberOfWeeks;
+        }
+         
+        
+        //Class within a class and a constructor example
+        
+        public class Manager : Employee
+        {
+            public Manager(int annualSalary) : base(annualSalary)
+                {
+                     //Add further instructions here.
+                }
+        }
+
+
+
+    }
+}
