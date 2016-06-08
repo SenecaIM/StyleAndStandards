b520d24900b93dee5c8d9d6f81f364e4bf6c5b5e
Kacper <kacper.lagowski@senecaim.com>
2016-06-08 14:26:00 +0100

    If statement
    no doubt.

--- Iterations and Loops.cs
+++ Iterations and Loops.cs
@@ -8,5 +8,27 @@ namespace Coding_Standard
 {
     class Iterations_and_Loops
     {
+        
+        
+        
+        //if statement example
+        if (condition)
+        {
+            action
+        }
+        else if(condition)
+        {
+            action
+        }
+        else
+        {
+            action
+        }
+        
+        //Short if statement example
+        if (condition) {action}
+        
+        
+        
     }
 }
