f2d4f087812bcb91b87c8bb74190407b8339d54d
Kacper <kacper.lagowski@senecaim.com>
2016-06-08 14:44:00 +0100

    Name Changing

---
+++ ControlsNaming.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Standard
+{
+        class Controls_naming
+    {
+        
+        //Controls naming will have a certain rule
+        //Each control will have a variable prefix 
+        //But also every control will be named depending on the type of the control
+         Examples: portfolioButton; configurationListView; setupLabel; transactionOLV 
+        //For long names of the controls, shortcuts will be used. Example : transactionOLV instead of transactionObjectListView
+         void portfolioButton_Click(Object sender,EventArgs e)
+         {
+             //Actions
+         }
+         
+         
+         //Arrays examples:
+         
+         int[] numbers = new int[5] {1, 2, 3, 4, 5};
+         //or
+         string[] names = new string[3] {"Matt", "Joanne", "Robert"};
+         
+    }
+}
