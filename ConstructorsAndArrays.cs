efd48e3f104c3344957501d2d3e9cebafc135b7a
Kacper <kacper.lagowski@senecaim.com>
2016-06-13 16:31:34 +0100

    Arrays added, constructors updated

--- ConstructorsAndArrays.cs
+++ ConstructorsAndArrays.cs
@@ -25,6 +25,31 @@ namespace Coding_Standard
                      //Add further instructions here.
                 }
         }
+        
+        
+        //Arrays examples
+        
+        
+        // Single-dimensional array
+        int[] numbers = new int[5];
+
+        // Multidimensional array
+        string[,] names = new string[5,4];
+
+        // Array-of-arrays (jagged array)
+        byte[][] scores = new byte[5][];
+
+        // Create the jagged array
+        for (int i = 0; i < scores.Length; i++)
+        {
+            scores[i] = new byte[i+3];
+        }
+
+        // Print length of each row
+        for (int i = 0; i < scores.Length; i++)
+        {
+            Console.WriteLine("Length of row {0} is {1}", i, scores[i].Length);
+        }
 
 
 
